052f2afc69530d2a8cbef2607c15bb3457231302
guibrok <gbroquet@gmail.com>
2026-02-20 14:31:53 +0100

    feat: update socket connection to support production environment

--- web/src/socket.ts
+++ web/src/socket.ts
@@ -1,5 +1,9 @@
 import { io } from 'socket.io-client'
 
-const socket = io('http://localhost:3001')
+const isProd = import.meta.env.PROD
+
+const socket = io(isProd ? '' : 'http://localhost:3001', {
+  path: '/socket.io'
+})
 
 export default socket
