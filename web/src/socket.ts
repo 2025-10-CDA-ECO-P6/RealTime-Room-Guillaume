dc71f239317cf09085b03ea7d8648883710778a0
guibrok <gbroquet@gmail.com>
2026-02-20 14:43:55 +0100

    Revert "feat: update socket connection to support production environment"
    This reverts commit 052f2afc69530d2a8cbef2607c15bb3457231302.

--- web/src/socket.ts
+++ web/src/socket.ts
@@ -1,9 +1,5 @@
 import { io } from 'socket.io-client'
 
-const isProd = import.meta.env.PROD
-
-const socket = io(isProd ? '' : 'http://localhost:3001', {
-  path: '/socket.io'
-})
+const socket = io('http://localhost:3001')
 
 export default socket
